30c0687f0b6b621d176d3a0c12729cefd961983f
CroatiaParanoia <1369248927@qq.com>
2021-02-26 23:49:23 +0800

    refactor: add some simple components

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,65 @@
 <template>
   <div id="app">
-    <SelectorContainer v-model="form">
+    <SelectorContainer v-model="form1">
       <Selector value-key="demo1" :multiply="false" />
       <Selector value-key="demo2" />
-      form: {{ form }}
-      <div @click="form = {}">RESET form</div>
+      <JsonView keyName="form1" :jsonValue="form1" />
+      <button @click="form1 = {}">RESET form</button>
     </SelectorContainer>
 
+    <UnderLine />
+
+    <SelectorContainer v-model="form2">
+      <Selector value-key="demo1" :multiply="false" />
+      <Selector value-key="demo2" />
+      <JsonView keyName="form2" :jsonValue="form2" />
+      <button @click="form2 = {}">RESET form</button>
+    </SelectorContainer>
+
+    <UnderLine />
+
     <Selector v-model="selectedValues" />
-    selectedValues: {{ selectedValues }}
-    <div @click="selectedValues = []">RESET selectedValues</div>
+    <JsonView keyName="selectedValues" :jsonValue="selectedValues" />
+    <button @click="selectedValues = []">RESET selectedValues</button>
   </div>
 </template>
 
 <script>
 import SelectorContainer from "./components/Selector/SelectorContainer";
 import Selector from "./components/Selector/Selector";
+
+const UnderLine = {
+  render(createElement) {
+    return createElement("div", {
+      style: "width: 100%; height: 1px; background-color: #ccc; margin: 24px 0",
+    });
+  },
+};
+const JsonView = {
+  render(createElement) {
+    return createElement("div", {}, [
+      this.keyName,
+      createElement("pre", {}, JSON.stringify(this.jsonValue, null, 2)),
+    ]);
+  },
+  props: {
+    keyName: String,
+    jsonValue: Object,
+  },
+};
 export default {
   name: "App",
   components: {
     SelectorContainer,
     Selector,
+    UnderLine,
+    JsonView,
   },
   data() {
     return {
       selectedValues: [],
-      form: {},
+      form1: {},
+      form2: {},
     };
   },
   methods: {},
@@ -33,12 +67,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
